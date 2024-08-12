fe8159282e2305b4576faee9d2315fadff43220a
Lucas Sousa <sousa.slucas@hotmail.com>
2024-08-12 15:27:49 -0300

    Position, Size, Minimize, Quit

--- src/main/java/testes/TesteGoogle.java
+++ src/main/java/testes/TesteGoogle.java
@@ -1,14 +1,23 @@
 package testes;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
 	
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
 		WebDriver driver = new FirefoxDriver();
+		//driver.manage().window().setPosition(new Point(100, 100));
+		//driver.manage().window().setSize(new Dimension(800, 600));
+		driver.manage().window().minimize();
 		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
 	}
 
 }
