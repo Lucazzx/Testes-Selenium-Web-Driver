aab335bf270c98258175be6fb2580b303afd415f
Lucas Sousa <sousa.slucas@hotmail.com>
2024-09-05 18:16:42 -0300

    Elementos Básicos

--- src/main/java/testes/TesteCampoTreinamentoElementosBasicos.java
+++ src/main/java/testes/TesteCampoTreinamentoElementosBasicos.java
@@ -18,7 +18,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class TesteCampoTreinamento {
+public class TesteCampoTreinamentoElementosBasicos {
 	
 	WebDriver driver;
 	String idElemento;
